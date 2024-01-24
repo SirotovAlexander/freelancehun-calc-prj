d56448ceb2d1dc4fe45638b1014b4694ed112df3
SirotovAlexander <alexander.sirotov@gmail.com>
2024-01-24 22:16:01 +0200

    Update formListener.js

--- formListener.js
+++ formListener.js
@@ -10,11 +10,19 @@ form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
+  const divText = document.querySelector(".product-price__item").textContent;
+  let price = 0;
+  if (divText.includes("350Lei")) {
+    price = 350;
+  } else {
+    price = 18;
+  }
+
   const form = event.target;
   const h = Number(form.elements.height.value);
   const w = Number(form.elements.width.value);
 
-  const r = Math.round(h * w * 0.0001 * 18);
+  const r = Math.round(h * w * 0.0001 * price);
   form.elements.height.value = "";
   form.elements.width.value = "";
   form.elements.result.value = r;
